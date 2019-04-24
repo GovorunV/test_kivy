28ba9b2a0212cd2f9c7c38cf26a343754e712b8e
GovorunV <govorun.vlad@gmail.com>
2019-04-24 15:44:26 +0300

    add layaut and scarlet

--- test_kivy.py
+++ test_kivy.py
@@ -1,22 +1,30 @@
 from kivy.app import App
 from kivy.uix.button import Button #создание кнопки
 
-from kivy.config import Config
-
+from kivy.config import Config #изменение размера окна
+from kivy.uix.scatter import Scatter  #дабавление емуляции тоуч
+from kivy.uix.floatlayout import FloatLayout
 Config.set('graphics','resizable','0') #запретить изменение размера окна
 Config.set('graphics','width','640') #размер окна
 Config.set('graphics','height','480') #размер окна
 
-from kivy.uix.floatlayout import FloatLayout
+
+
 
 class MyApp(App):
-    def build(self):#создание кнопки
-        fl=FloatLayout(size=(300,300))
-        return Button(text="Надпись",
-                      font_size=40,
-                      on_press=self.btn_press,# что происходит после нажатие кнопки
-                      background_color = [1,0,0,1],
-                      background_normal = "")
+    def build(self):
+        ss=FloatLayout(size= (350,350))
+        ss.add_widget(Button( #создание кнопки
+            text="Надпись",
+            font_size=40,
+            on_press=self.btn_press,# что происходит после нажатие кнопки
+            background_color = [1,0,0,1],
+            background_normal = "",
+            size_hint=(.5, .25),
+            pos = (640/2-(640*.5/2), 480/2-(480*.25/2)))) #располождение кнопки по центру)
+        sc = Scatter() #дабавление емуляции тоуч
+        sc.add_widget(ss) #дабавление емуляции тоуч
+        return sc
     def btn_press(self,instance): #функция которая срабатывает после нажатия
         print("кнопка нажата") #вывод в консоли после нажатия
         instance.text = "Нажата1" #смена надписи кнопки
